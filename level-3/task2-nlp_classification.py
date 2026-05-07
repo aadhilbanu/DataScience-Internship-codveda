bc67c1d83a227d4fd3c23ff4d25d10fb74be9dde
aadhilbanu <aadhilbanu139@gmail.com>
2026-05-07 23:43:13 +0530

    Create task2-nlp_classification.py

---
+++ level-3/task2-nlp_classification.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("sentiment dataset.csv")
+
+print(df.head())
+
+# Features and target
+X = df['text']
+y = df['label']
+
+# Convert text into numerical format
+vectorizer = TfidfVectorizer()
+X_vectorized = vectorizer.fit_transform(X)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vectorized, y, test_size=0.2
+)
+
+# Train model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Prediction
+y_pred = model.predict(X_test)
+
+# Accuracy
+print("Accuracy:", accuracy_score(y_test, y_pred))
